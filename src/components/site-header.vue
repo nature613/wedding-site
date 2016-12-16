7b73bbd6d60cbfa8c728fb81f0b50f618196bdd9
nature613 <chikonakamura613@gmail.com>
2016-12-15 19:58:50 -0600

    update header and footer and added cards

--- src/components/site-header.vue
+++ src/components/site-header.vue
@@ -1,22 +1,20 @@
 <template>
 	<div id="header">
-		<div class="container">
-			<div class="row">
-				<b-navbar fixed="top" type="dark" variant="default" full>
-					<div class="col-lg-4">
-						<a class="navbar-brand" href="#"><h1>David <span>&amp;</span> Dacia</h1></a>
-					</div>
-					<div class="col-lg-8">
-						<b-nav type="navbar" class="pull-xs-left">
-							<b-nav-item link="#" active>Home</b-nav-item>
-							<b-nav-item link="#">Wedding</b-nav-item>
-							<b-nav-item link="#">RSVP</b-nav-item>
-							<b-nav-item link="#">Registry</b-nav-item>
-							<b-nav-item link="#">While in Chicago</b-nav-item>
-						</b-nav>
-					</div>
-				</b-navbar>
-			</div>
+		<div class="row">
+			<b-navbar fixed="top" type="dark" variant="default" full>
+				<div class="col-lg-4">
+					<a class="navbar-brand" href="#"><h1>David <span>&amp;</span> Dacia</h1></a>
+				</div>
+				<div class="col-lg-8">
+					<b-nav type="navbar" class="pull-xs-left">
+						<b-nav-item link="#" active>Home</b-nav-item>
+						<b-nav-item link="#">Wedding</b-nav-item>
+						<b-nav-item link="#">RSVP</b-nav-item>
+						<b-nav-item link="#">Registry</b-nav-item>
+						<b-nav-item link="#">While in Chicago</b-nav-item>
+					</b-nav>
+				</div>
+			</b-navbar>
 		</div>
 	</div>
 </template>
@@ -30,6 +28,9 @@
 	#header
 		background: $background3;
 		opacity: .8;
+
+		.bg-faded
+			background: transparent;
 
 		.nav
 			float: right;
